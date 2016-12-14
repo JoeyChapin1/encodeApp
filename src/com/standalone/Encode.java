62d5743ac6d7c4f75895a6a1c40562916c7ad70c
JoeyChapin1 <joey.chapin@appian.com>
2016-12-14 16:18:57 -0500

    Final Changes, fully working encoding application

--- src/com/standalone/Encode.java
+++ src/com/standalone/Encode.java
@@ -9,6 +9,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FilenameUtils;
+
 
 
 
@@ -26,83 +28,73 @@ public class Encode {
 		}
 		
 		// Make filepath entered relative to cwd
-		String filePath = Paths.get(".").toAbsolutePath().normalize().toString() + File.separator;
+		String parentPath = Paths.get(".").toAbsolutePath().normalize().toString() + File.separator;
+		String filePath = parentPath;
 		
-		// Check to see if Encoded.txt already exists in cwd
-		String encodedPath = filePath + "Encoded.txt";
-		 
-		// Can be used to change name of filePath -- Which is more elegant? Allow them to choose file path name/whether they want to overwrite?
-/*		String newName = "";
-		while (new File(encodedPath).exists()) {
-			option = c.readLine("File: " + encodedPath + " already exists. Is it okay to overwrite? ['y' to overwrite, 'n' to change file name]");
-			if (option.equals("y")) {
-				break;
-			}
-			if (option.equals("n")) {
-				newName = c.readLine("Enter new file name: ");
-				encodedPath = Paths.get(".").toAbsolutePath().normalize().toString() + File.separator + newName;
-			}
-		}*/
-		
-		Integer version = 1;
-		if (new File(encodedPath).exists()) {
-			String tempPath = Paths.get(".").toAbsolutePath().normalize().toString() + File.separator + "Encoded (1).txt";
-			while (new File(tempPath).exists()) {
-				tempPath = Paths.get(".").toAbsolutePath().normalize().toString() + File.separator + "Encoded (" + version.toString() + ").txt";;
-				version += 1;
-			}
-			encodedPath = tempPath;
-		}
-					
 		filePath += c.readLine(ENTER_PATH);
 		
 		File plainTextFile = new File(filePath);
 		
-		// Continue to loop until user quits or enters path to file that exists
+		// If file does not exist, continue to loop until user quits or enters path to file that exists
 		if (!plainTextFile.exists()) {
 			option = c.readLine(FILE_DOES_NOT_EXIST_OPTIONS);
 			while (!plainTextFile.exists()) {	
 				if (option.equals("f")) {
-					filePath = Paths.get(".").toAbsolutePath().normalize().toString() + File.separator;
-					filePath += c.readLine("\n" + ENTER_PATH);
+					filePath = parentPath + c.readLine("\n" + ENTER_PATH);
 					plainTextFile = new File(filePath);
 					if (!plainTextFile.exists()) {
 						option = c.readLine(FILE_DOES_NOT_EXIST_OPTIONS);
 					}
 				}
-				
 				else if (option.equals("q")) {
 					System.out.println("\nNo file will be encoded. Exiting application...");
 					return;
 				}
-				
 				else {
 					option = c.readLine("\nInput: \'" + option + "\' invalid." + FILE_DOES_NOT_EXIST_OPTIONS);
 				}
 			}
 		}
 		
+		// Check to see if Encoded.txt already exists in cwd
+		String encodedPathBase = parentPath + FilenameUtils.removeExtension(plainTextFile.getName());
+		String encodedPath = encodedPathBase + "_encoded.txt";
+		
+		Integer version = 1;
+		if (new File(encodedPath).exists()) {
+			encodedPath = encodedPathBase + "_encoded (1).txt";
+			while (new File(encodedPath).exists()) {
+				encodedPath = encodedPathBase + "_encoded (" + version.toString() + ").txt";;
+				version += 1;
+			}
+		}
+		
 		File parent = plainTextFile.getParentFile();
 		Path path = Paths.get(plainTextFile.getAbsolutePath());
 		
+		// Read in bytes and encode
 		byte[] byteArray = (Files.readAllBytes(path));
 		byte[] encoded = Base64.encodeBase64(byteArray);
 		
+		// Stream output to new file in cwd, named original filename + "_encoded.txt"
 		FileOutputStream out = null;
 		if (parent.equals(null)) {
-			out = new FileOutputStream("Encoded.txt");
+			out = new FileOutputStream(encodedPath);
 		} else {
 			System.out.println("File was created at: " + encodedPath);
 			out = new FileOutputStream(encodedPath);
 			
 		}
 		
-		// Allow user to open the file upon encoding if they wish (eases copy/paste)
-		option = c.readLine("\nType \'o\' to open the file upon encoding. [Return to skip]: ");
+		// Allow user to open the file upon encoding if on Windows OS if they wish (eases copy/paste)
+		if(isWindows(System.getProperty("os.name"))) {
+			option = c.readLine("\nType \'o\' to open the file upon encoding. [Return to skip]: ");
+		}
 			
 		out.write(encoded);
 		out.close();
 		
+		// If user is on Windows OS, allow them to open the file upon encoding if so choose
 		if ((option.equals("o") || option.equals("O")) && new File(encodedPath).exists()) {
 			Process p = Runtime.getRuntime().exec("rundll32 url.dll, FileProtocolHandler " + encodedPath);
 			p.waitFor();
@@ -110,4 +102,8 @@ public class Encode {
 		
 		System.out.println("Application closed.");
 	}
+	
+	public static boolean isWindows(String os) {
+        return (os.toLowerCase().contains("windows"));
+    }
 }
